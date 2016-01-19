779bc47a05258cd2220ef62a99bebefdd9afdd87
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-01-19 10:19:39 -0700

    started 3

---
+++ src/exersise3.java
@@ -0,0 +1,25 @@
+
+public class exersise3 {
+	
+
+	
+	{
+
+	  public static void main ( String[] args )
+	  {
+	    int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
+	    
+	    // declare and initialize variables
+	    
+	    
+	    // find the element nearest to zero
+	    for (  )
+	    {
+	    }
+	      
+	    // write out the element nearest to zero
+	    System.out.println(  );
+
+	  }
+	}      
+}
